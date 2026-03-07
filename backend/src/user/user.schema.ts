16ac345901d53c77714be22277717220c8e8a2b5
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2026-03-07 14:05:22 +0200

    build: user sechema

--- backend/src/user/user.schema.ts
+++ backend/src/user/user.schema.ts
@@ -5,14 +5,52 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop()
+  @Prop({
+    required: true,
+    type: String,
+    min: [3, 'Name must be at least 3 characters long'],
+    max: [30, 'Name must be at most 30 characters long'],
+  })
   name: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, unique: true })
   email: string;
 
-  @Prop({ required: true })
+  @Prop({
+    required: true,
+    min: [8, 'Password must be at least 8 characters long'],
+    max: [20, 'Password must be at most 20 characters long'],
+  })
   password: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  })
+  role: string;
+
+  @Prop({ type: String })
+  avatar?: string;
+
+  @Prop({ type: Number, min: 18, max: 100 })
+  age: number;
+
+  @Prop({ type: String })
+  phoneNumber: string;
+
+  @Prop({ type: String })
+  address: string;
+
+  @Prop({ type: Boolean, enum: [true, false] })
+  active: boolean;
+
+  @Prop({ type: String })
+  verificationCode: string;
+
+  @Prop({ type: String, enum: ['male', 'female'] })
+  gender: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './user.schema';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.userModel.create(createUserDto);
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userModel.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.userModel.findById(id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return 'Update user';
+    return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return 'Remove user';
+    return this.userModel.findByIdAndDelete(id);
   }
 }
