9271a11f8f2e6cf383e2a0d3d3de20e8398b0026
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2026-04-11 11:34:50 +0200

    build: product CRUD functionality

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-
+import { UseGuards } from '@nestjs/common';
+import { Roles } from '@/user/decorator/roles.decorator';
+import { UserRole } from '@/user/enums/user-role.enum';
+import { AuthGuard } from '@/user/guard/auth.guard';
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
-
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
-  }
 
   @Get()
   findAll() {
@@ -19,16 +25,27 @@ export class ProductController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
+    return this.productService.findOne(id);
   }
 
+  @Roles([UserRole.ADMIN])
+  @UseGuards(AuthGuard)
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productService.create(createProductDto);
+  }
+
+  @Roles([UserRole.ADMIN])
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
+    return this.productService.update(id, updateProductDto);
   }
 
+  @Roles([UserRole.ADMIN])
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
+    return this.productService.remove(id);
   }
 }
--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Product, ProductDocument } from './product.schema';
 import { Model } from 'mongoose';
+import { findDocumentById } from '@/common/utils/find-by-id.util';
+import { escapeRegex } from '@/common/utils/escape-regex.util';
 
 @Injectable()
 export class ProductService {
@@ -12,23 +14,67 @@ export class ProductService {
     private readonly productModel: Model<ProductDocument>,
   ) {}
 
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
-
   findAll() {
-    return `This action returns all product`;
+    const products = this.productModel.find().exec();
+
+    return {
+      data: products,
+      message: 'Products fetched successfully',
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await findDocumentById(this.productModel, id, 'Product');
+
+    return {
+      message: 'Product fetched successfully',
+      data: product,
+    };
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async create(createProductDto: CreateProductDto) {
+    const isExist = await this.productModel.findOne({
+      name: {
+        $regex: `^${escapeRegex(createProductDto.title)}$`,
+        $options: 'i',
+      },
+    });
+
+    if (isExist) throw new BadRequestException('Product already exists');
+
+    const product = await this.productModel.create(createProductDto);
+
+    return {
+      message: 'Product created successfully',
+      data: product,
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    await findDocumentById(this.productModel, id, 'Product');
+
+    const updatedProduct = await this.productModel.findByIdAndUpdate(
+      id,
+      updateProductDto,
+      {
+        new: true,
+      },
+    );
+
+    return {
+      message: 'Product updated successfully',
+      data: updatedProduct,
+    };
+  }
+
+  async remove(id: string) {
+    const product = await findDocumentById(this.productModel, id, 'Product');
+
+    await product.deleteOne();
+
+    return {
+      message: 'Product deleted successfully',
+      data: product,
+    };
   }
 }
